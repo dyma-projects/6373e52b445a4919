ce12ab922077705a1c2ae1f414ee3c7d2903d25b
fz <phiane.d@gmail.com>
2023-01-14 16:37:03 +0100

    project 2 done

--- src/app/exercice2/exercice2.component.ts
+++ src/app/exercice2/exercice2.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Exercice2Component implements OnInit {
 
+  public click(){
+    console.log('click')
+  }
   constructor() { }
 
   ngOnInit() {
